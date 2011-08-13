9891fbd8ad42427471d93eab039e23708140d0ec
Matthew Burnett <matt@sowhatsthdeal.com>
2011-08-13 14:09:49 -0400

    more deploy stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,14 @@
-require 'capistrano/ext/multistage'
-set :default_stage, "production"
+require 'bundler/capistrano'
+# require 'capistrano/ext/multistage'
+# set :default_stage, "production"
 
 # set :application, "tamarindoflondon.com"
 set :application, "50.56.57.18"
+
+role :web, "50.56.57.18"                          # Your HTTP server, Apache/etc
+role :app, "50.56.57.18"                          # This may be the same as your `Web` server
+role :db,  "50.56.57.18", :primary => true # This is where Rails migrations will run
+
 set :user, "tamarind"
 set :repository,  "git@github.com:kaliara/tamarind.git"
 
@@ -10,7 +16,7 @@ set :deploy_to, "/home/tamarind/www"
 set :deploy_via, :copy
 
 set :port, 30000
-
+set :use_sudo, true
 set :scm, :git
 
 # If you are using Passenger mod_rails uncomment this:
@@ -22,6 +28,7 @@ namespace :deploy do
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    # run "cd #{deploy_to}/current && rvmsudo #{passenger_path}passenger start -e #{rails_env} -p #{passenger_port} -a 127.0.0.1 -d --pid-file #{release_path}\ /tmp/pids/passenger.#{passenger_port}.pid"
   end
 end
 

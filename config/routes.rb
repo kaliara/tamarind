4f092603c083365bffd263d9b19ab32613e91b49
Matthew Burnett <matt@sowhatsthdeal.com>
2011-10-05 01:33:06 -0400

    added opentable form

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 App::Application.routes.draw do
-  # match 'invite/:code'                 => 'subscribers#new',  :as => :invite
   match 'applicants/apply/:position'   => 'applicants#new',     :as => :new_applicant
   match 'admin/login'                  => 'admin#login',        :as => :admin_login 
   match 'admin/logout'                 => 'admin#logout',       :as => :admin_logout
   match 'admin'                        => 'admin#home',         :as => :admin_home 
   match 'menus/:name(.:format)'        => 'menus#show',         :as => :view_menu
   
-
-  resources :applicants,  :only => [:create]
-  resources :menus,       :only => [:index, :show]
-  resources :subscribers, :only => [:new, :create, :show]
+  resources :applicants,    :only => [:create]
+  resources :menus,         :only => [:index, :show]
+  resources :reservations,  :only => [:new]
+  resources :subscribers,   :only => [:new, :create, :show]
 
   namespace :admin do
     resources :applicants

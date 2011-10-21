a0eec9c82312fc5908127faa633e924256492372
Matthew Burnett <matt@sowhatsthdeal.com>
2011-10-21 09:44:33 -0400

    added wysiwyg editor

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,14 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	//config.contentscss = ?????????????
+	config.toolbar = 'Custom';
+	config.toolbar_Custom =
+  [
+  	['Bold', 'Italic','Underline','TextColor','Format','Font','FontSize', 'NumberedList', 'BulletedList','JustifyLeft','JustifyCenter','JustifyRight','Blockquote', '-', 'Link', 'Unlink','-','Image','-','RemoveFormat','Source']
+  ];
+};

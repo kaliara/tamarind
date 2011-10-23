949696c0bffeac65b26d292dbb403e2b123a55e4
Matthew Burnett <matt@sowhatsthdeal.com>
2011-10-22 23:22:09 -0400

    fixed up the cms editor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -5,10 +5,12 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 CKEDITOR.editorConfig = function( config )
 {
-	//config.contentscss = ?????????????
 	config.toolbar = 'Custom';
-	config.toolbar_Custom =
-  [
-  	['Bold', 'Italic','Underline','TextColor','Format','Font','FontSize', 'NumberedList', 'BulletedList','JustifyLeft','JustifyCenter','JustifyRight','Blockquote', '-', 'Link', 'Unlink','-','Image','-','RemoveFormat','Source']
-  ];
+	config.toolbar_Custom = [['Bold', 'Italic','Underline','TextColor','Format','Font','FontSize', 'NumberedList', 'BulletedList','JustifyLeft','JustifyCenter','JustifyRight','Blockquote', '-', 'Link', 'Unlink','-','Image','-','RemoveFormat','Source']];
+  config.contentsCss = '/assets/application.css';
+  config.toolbarCanCollapse = false;
+  config.bodyClass = 'ckeditor';
+  config.dialog_backgroundCoverColor = '#000';
+  config.disableNativeSpellChecker = false;  
+  config.width = 850;
 };

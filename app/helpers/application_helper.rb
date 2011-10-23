5a7d0c983affcc8af9dd8dac9dea96e4119664c3
Matthew Burnett <matt@sowhatsthdeal.com>
2011-10-23 00:13:43 -0400

    made applicants much easier to review

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def nice_boolean(exp)
+    exp == false ? "No" : "Yes"
+  end
+  
+  def nice_notes(str)
+    str.gsub(/\-\s+|\,\s+|\.\s+|\;/,"<br/>").html_safe
+  end
+  
+  def nice_date(date)
+    date.strftime("%b %e, %Y")
+  end
 end

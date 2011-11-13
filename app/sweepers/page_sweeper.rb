1b234a40c16973f20b079eeed60464c7c5f4ab40
Matthew Burnett <matt@sowhatsthdeal.com>
2011-11-13 15:31:08 -0500

    initial setup of sweepers

--- app/sweepers/page_sweeper.rb
+++ app/sweepers/page_sweeper.rb
@@ -14,6 +14,6 @@ class PageSweeper < ActionController::Caching::Sweeper
 
 private
   def expire_cache_for(page)
-    expire_action(:controller => "pages", :action => "show", :id => page.id)
+    expire_page(:controller => 'pages', :action => page.slug)
   end
 end
